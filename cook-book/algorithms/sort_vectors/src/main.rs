eb33bc167725a15e1a0c2003d7c01d2d8b502975
Diehard073055 <eshanshafeeq073055@gmail.com>
2022-04-14 09:45:38 +0800

    add(cook-book/algorithms/sort_vector)
      - sort a vec of integers.
      - sort a vec of floats.
      - sort a vec of structs.

---
+++ cook-book/algorithms/sort_vectors/src/main.rs
@@ -0,0 +1,61 @@
+fn sort_a_vec_of_int(){
+    let mut vec = vec![1, 5, 10, 2, 15];
+    vec.sort();
+    assert_eq!(vec, vec![1, 2, 5, 10, 15]);
+}
+
+fn sort_a_vec_of_floats(){
+    let mut vec = vec![1.1, 1.15, 5.5, 1.123, 2.0];
+    vec.sort_by(|a, b| a.partial_cmp(b).unwrap());
+    assert_eq!(vec, vec![1.1, 1.123, 1.15, 2.0, 5.5]);
+}
+
+// sort a vec of structs
+
+#[derive(Debug, Eq, Ord, PartialEq, PartialOrd)]
+struct Person {
+    name: String,
+    age: u32,
+}
+
+impl Person {
+    pub fn new(name: String, age: u32) -> Self {
+        Person {name, age}
+    }
+}
+
+fn sort_a_vec_of_struct(){
+    let mut people = vec![
+        Person::new("Zoe".to_string(), 25),
+        Person::new("Al".to_string(), 60),
+        Person::new("John".to_string(), 1),
+    ];
+
+    // sort people by Name and age
+    people.sort();
+
+    assert_eq!(
+        people,
+        vec![
+            Person::new("Al".to_string(), 60),
+            Person::new("John".to_string(), 1),
+            Person::new("Zoe".to_string(), 25),
+        ]);
+    
+    // sort people by age only
+    people.sort_by(|a, b| b.age.cmp(&a.age));
+
+    assert_eq!(
+        people,
+        vec![
+            Person::new("Al".to_string(), 60),
+            Person::new("Zoe".to_string(), 25),
+            Person::new("John".to_string(), 1),
+        ]);
+}
+
+fn main() {
+    sort_a_vec_of_int();
+    sort_a_vec_of_floats();
+    sort_a_vec_of_struct();
+}
