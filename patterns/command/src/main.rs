ae7f094a8d1fdc480781af6001c12366b36bdcff
Diehard073055 <eshanshafeeq073055@gmail.com>
2022-05-03 21:44:39 +0800

    add(command_pattern): implement the first example of command pattern

---
+++ patterns/command/src/main.rs
@@ -0,0 +1,65 @@
+// migration trait
+pub trait Migration {
+    fn execute(&self) -> &str;
+    fn rollback(&self) -> &str;
+}
+
+// create table command
+pub struct CreateTable;
+impl Migration for CreateTable {
+    fn execute(&self) -> &str {
+        "create table"
+    }
+    fn rollback(&self) -> &str {
+        "drop table"
+    }
+}
+// add field command
+pub struct AddField;
+impl Migration for AddField {
+    fn execute(&self) -> &str {
+        "add field"
+    }
+    fn rollback(&self) -> &str {
+        "remove field"
+    }
+}
+
+struct Schema {
+    commands: Vec<Box<dyn Migration>>,
+}
+
+impl Schema {
+    fn new() -> Self {
+        Self { commands: vec![] }
+    }
+
+    fn add_migration(&mut self, cmd: Box<dyn Migration>) {
+        self.commands.push(cmd);
+    }
+
+    fn execute(&self) -> Vec<&str> {
+        self.commands.iter().map(|cmd| cmd.execute()).collect()
+    }
+
+    fn rollback(&self) -> Vec<&str> {
+        self.commands
+            .iter()
+            .rev()
+            .map(|cmd| cmd.rollback())
+            .collect()
+    }
+}
+fn main() {
+    let mut schema = Schema::new();
+
+    let cmd = Box::new(CreateTable);
+    schema.add_migration(cmd);
+    let cmd = Box::new(AddField);
+    schema.add_migration(cmd);
+
+    println!("running migration tests");
+    assert_eq!(vec!["create table", "add field"], schema.execute());
+    assert_eq!(vec!["remove field", "drop table"], schema.rollback());
+    println!("done with migration tests");
+}
